810c84ec109f1b88aeddd126fd27f554f7eba865
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-01 11:53:09 +0200

    add router to server app

--- libarary-mine/server.js
+++ libarary-mine/server.js
@@ -2,27 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import { getAllBooks, getBookById } from './db.js';
 import { db } from './src/db/db.js';
+import router from './src/routes/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(db);
-app.get('/api/books', async (req, res) => {
-	try {
-		const books = await getAllBooks();
-		res.json(books);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ error: 'Failed to retrieve books' });
-	}
+app.use(function (req, res, next) {
+	req.db = db;
+	next();
 });
+
+app.use(router);
 
 app.listen(3001, () => {
 	console.log('Server up on 3001');
-});
-
-app.get('/book', (req, res) => res.send('Nu zdarova'));
-app.get('/bookz', async (req, res) => {
-	const books = await getAllBooks();
-	res.json(books[0].id);
 });
