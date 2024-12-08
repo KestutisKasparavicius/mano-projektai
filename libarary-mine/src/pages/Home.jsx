514bf2a6864a24e661278b76eedec7695648a91b
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-08 17:25:05 +0200

    add fetching from api

--- libarary-mine/src/pages/Home.jsx
+++ libarary-mine/src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from 'react'
 
 const Home = () => {
-  
+  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+
+  useEffect(() => {
+    
+      const fetchData = async () => {
+        const response = await fetch("http://localhost:3001/api/book");
+        const resData = await response.json();
+        setData(resData);
+      }
+      fetchData()
+      }, []
+  )
   return (
     <div>
-    elo
+    <ul>
+      {data.map((obj) => {
+        return <li key={obj.autorius}>{obj.pavadinimas}</li>
+      })}
+    </ul>
     </div>
   )
 }
