9a12f4650fe5ba074458f3a7f3ad3a2eab297b45
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-09 20:45:24 +0200

    add env variables to server

--- libarary-mine/src/db/db.js
+++ libarary-mine/src/db/db.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2';
 
 export const db = mysql
 	.createPool({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'knygos',
+		host: process.env.HOST,
+		user: process.env.DB_USER,
+		password: process.env.PASSWORD,
+		database: process.env.DATABASE,
 	})
 	.promise();
