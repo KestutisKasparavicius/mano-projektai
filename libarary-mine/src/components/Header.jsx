384075bd9518aae6f837342922a16bd75aa8b0f0
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-13 22:59:04 +0200

    add header component

---
+++ libarary-mine/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Novel</a></li>
+                <li><a href="#">Romance</a></li>
+                <li><a href="#">Sci-fi</a></li>
+                <li><a href="#">Self Help</a></li>
+            </ul>
+        </nav>
+    </header>
+
+  )
+}
+
+export default Header
