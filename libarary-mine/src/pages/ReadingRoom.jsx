9df87ed3ed5da70df2ba86348676782d84b85e4d
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-17 20:31:20 +0200

    rename for Auditorium

---
+++ libarary-mine/src/pages/ReadingRoom.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes, { object } from 'prop-types'
+
+const ReadingRoom = (bookObj) => {
+  const [book, setBook] = useState([]);
+
+
+  return (
+    {
+      if (book) {
+        return <div></div>
+      }
+    }
+  )
+}
+
+ReadingRoom.propTypes = {object}
+
+export default ReadingRoom
