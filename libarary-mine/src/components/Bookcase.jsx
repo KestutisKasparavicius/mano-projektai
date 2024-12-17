399d1b703f80557a24618c83a4de6a1dabd9d00d
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-17 20:47:49 +0200

    replace previous book loading logic with designated components

--- libarary-mine/src/components/Bookcase.jsx
+++ libarary-mine/src/components/Bookcase.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 
-const Bookcase = ({bookList}) => {
+const Bookcase = ({children}) => {
   return (
     <main>
     <div className="bookcase">
   <div className='bookshelf-container'>
-    <div className="bookshelf">
-      {bookList.map((item) => {
-        return ( 
-        <div className="book" key={item.id}> {item.pavadinimas} </div>
-      )
-      })}
-    </div>
-    <div className="bookshelf"></div>
-    <div className="bookshelf"></div>
+    {children}
   </div>
   <div className='cabinet'>
     <span className="door">&#128996;</span>
