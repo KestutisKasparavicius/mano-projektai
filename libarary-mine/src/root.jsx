793fce8ab48cbb34eea42653f9a7b8af2438d8af
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-19 22:08:47 +0200

    add basic routing

--- libarary-mine/src/root.jsx
+++ libarary-mine/src/root.jsx
@@ -1,14 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import './styles/index.css'
 import './styles/normalize.css'
-import App from './App.jsx'
+import ReadingRoom from './pages/ReadingRoom.jsx';
+import Home from './pages/Home.jsx';
+import Book from './components/Book.jsx';
+import Bookshelf from './components/Bookshelf.jsx';
 
 
+const root = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}>
+  </Route>
+
+  <Route path='reading-room' element={<ReadingRoom/>}>
+    <Route path=':categoryID' element={<Bookshelf/>}/>
+    <Route path=':categoryID/:bookID'element={<Book/>}/>
+  </Route>
+
+ 
+</Routes>
+  </BrowserRouter>
 );
