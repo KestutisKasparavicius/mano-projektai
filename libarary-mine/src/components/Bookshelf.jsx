9aa3669a8864bebdc84696bab3d91f94c9354617
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-25 16:51:41 +0200

    update router, update styles

--- libarary-mine/src/components/Bookshelf.jsx
+++ libarary-mine/src/components/Bookshelf.jsx
@@ -2,15 +2,30 @@ import React from 'react'
 import BookshelfCSS from '../styles/bookshelf.module.css'
 const Bookshelf = ({bookList, genre}) => {
   return (
-    <label htmlFor={BookshelfCSS.bookshelf}> {genre}
+    <div> 
+    <span className={BookshelfCSS.genre}>{genre}</span>
     <ul className={BookshelfCSS.bookshelf}>
     {bookList.map((item) => {
       return ( 
-      <li className={BookshelfCSS.book} key={item.id}> {item.pavadinimas} </li>
+      <li className={BookshelfCSS.book} key={item.id}>
+        {item.pavadinimas} 
+        <span className={BookshelfCSS.tooltip} title="description">
+          {item.pavadinimas}
+          <br />
+          {item.autorius}
+          <br />
+          {item.puslapiai} puslapiai
+          <br />
+          
+          <p className={BookshelfCSS.description}>
+            {item.aprasimas}
+          </p>
+        </span>
+        </li>
     )
     })}
   </ul>
-  </label>
+  </div>
   )
 }
 Bookshelf.propTypes = {Object, String}
