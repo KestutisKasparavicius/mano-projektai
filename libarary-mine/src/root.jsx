4efd1064f8e608f029e513605a4d8f9a324076bb
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2024-12-27 17:52:53 +0200

    add routes

--- libarary-mine/src/root.jsx
+++ libarary-mine/src/root.jsx
@@ -1,5 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
@@ -8,19 +6,19 @@ import './styles/normalize.css'
 import ReadingRoom from './pages/ReadingRoom.jsx';
 import Home from './pages/Home.jsx';
 import Book from './components/Book.jsx';
-import Bookshelf from './components/Bookshelf.jsx';
+import BookAbout from './pages/BookAbout.jsx';
 
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-<Routes>
-  <Route path='/' element={<Home/>}>
-  </Route>
+<Routes> 
 
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about/:categoryID/:bookID' element={<BookAbout/>}/>
+    
   <Route path='reading-room' element={<ReadingRoom/>}>
-    <Route path=':categoryID' element={<Bookshelf/>}/>
     <Route path=':categoryID/:bookID'element={<Book/>}/>
   </Route>
 
