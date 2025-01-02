c9fd1bb8ab52f2d1a6e85e32aafc3da61240e364
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2025-01-02 21:08:29 +0200

    add book preview

--- libarary-mine/src/components/Book.jsx
+++ libarary-mine/src/components/Book.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { useOutletContext } from 'react-router'
+import React from 'react';
+import { useOutletContext, Link } from 'react-router';
+import Images from '../assets/Images';
+import Image from '../components/Image';
+import BookCSS from '../styles/book.module.css';
 const Book = () => {
-  const [book] = useOutletContext()
-  return (
-    <div>{book.knygos_perziura}</div>
-  )
-}
+	const [book] = useOutletContext();
+	let content = book?.knygos_perziura
+		? book.knygos_perziura.split('<!-- PAGE -->')
+		: [];
+	return (
+		<div className={BookCSS.page}>
+			<main className={BookCSS.bookContainer}>
+				<section className={BookCSS.bookPage}>
+					<article className={BookCSS.content}>
+						<pre className={BookCSS.text}>{content[0]}</pre>
+					</article>
+				</section>
+				<section className={BookCSS.bookPage}>
+					<article className={BookCSS.content}>
+						<pre className={BookCSS.text}>{content[1]}</pre>
+					</article>
+				</section>
+				<nav className={BookCSS.navBar}>
+					<Link to={'/'}>
+						Back
+						<Image image={Images.back} classElem={BookCSS.icon} />
+					</Link>
+				</nav>
+			</main>
+		</div>
+	);
+};
 
-export default Book
+export default Book;
