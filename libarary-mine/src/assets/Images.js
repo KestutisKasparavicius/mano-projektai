f8d743d23ad33ce82a43841fa0ff9c71406428ff
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2025-01-02 21:07:10 +0200

    add image

--- libarary-mine/src/assets/Images.js
+++ libarary-mine/src/assets/Images.js
@@ -1,9 +1,10 @@
 import wallpaper from './bookstore4k.jpg';
 import burningBook from './burnt_books.png';
 import bookstackImage from './bookstack.png';
-
+import goBack from './goBack.png';
 export default {
 	bookstoreWallpaper: wallpaper,
 	bookstack: bookstackImage,
 	errorPhoto: burningBook,
+	back: goBack,
 };
