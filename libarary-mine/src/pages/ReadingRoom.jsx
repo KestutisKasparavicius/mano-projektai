4de78385af14c3b8bcb8d2b08a5c2ffb6452c374
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2025-01-02 21:09:04 +0200

    remove comment

--- libarary-mine/src/pages/ReadingRoom.jsx
+++ libarary-mine/src/pages/ReadingRoom.jsx
@@ -1,60 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes, { object } from 'prop-types'
+import React, { useEffect, useState } from 'react';
+import PropTypes, { object } from 'prop-types';
 import { Outlet, useParams } from 'react-router';
 
-const ReadingRoom = ({children}) => {
-  const { bookID } = useParams();
-  const [isLoading, setLoading] = useState(false);
-  const [err, setErr] = useState(false);
-  const [book, setBook] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-      const response = await fetch(`http://localhost:3001/api/book/${bookID}`);
-      const [resData] = await response.json();
-      console.log(resData)
-      setBook(resData)
-      } catch (err) {
-        setErr(err);
-      } finally {
-        setLoading(false)
-      };
-      
-      
-    }
-    fetchData()
-    }, [bookID]
-)
-    
-    
-  if (isLoading) {
-    return (
-    <div className="loading">Loading..</div>
-    )
-    }
-    
-    if (err) {
-    return (
-    <Error/>
-    )
-    }
-  
-  return (
-      //
-      // Cia children eina, ir per juos pareina parinkta knyga is bookselection. Manrods....
-      <>
-      <div className="book">Some book info</div>
-    
-      <Outlet context={[book]}/>
-      </>
-    
-  )
-}
+const ReadingRoom = ({ children }) => {
+	const { bookID } = useParams();
+	const [isLoading, setLoading] = useState(false);
+	const [err, setErr] = useState(false);
+	const [book, setBook] = useState([]);
 
-ReadingRoom.propTypes = {Object}
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				const response = await fetch(
+					`http://localhost:3001/api/book/${bookID}`
+				);
+				const [resData] = await response.json();
+				setBook(resData);
+			} catch (err) {
+				setErr(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, [bookID]);
 
-export default ReadingRoom      
+	if (isLoading) {
+		return <div className="loading">Loading..</div>;
+	}
 
+	if (err) {
+		return <Error />;
+	}
 
+	return (
+		//
+		// Cia children eina, ir per juos pareina parinkta knyga is bookselection. Manrods....
+		<>
+			<Outlet context={[book]} />
+		</>
+	);
+};
+
+ReadingRoom.propTypes = { Object };
+
+export default ReadingRoom;
