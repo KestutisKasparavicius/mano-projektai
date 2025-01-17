2e5a8becedc4b5ab18a1d3210721dfa235a151b9
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2025-01-17 23:32:36 +0200

    add class and basic generation

--- chess/src/components/Chessboard.tsx
+++ chess/src/components/Chessboard.tsx
@@ -4,17 +4,28 @@ import CSS from '../styles/ChessboardCSS.module.css'
 
 
 class Square {
-  constructor(active: boolean , colour: 'white' | 'black', file: string, rank: number, piece: 'pawn' | 'rook' | 'bishop' | 'knight' | 'queen' | 'king'){
-    
-  }
-
+  constructor(
+    public id?: number,
+    public active: boolean = false,
+    public white: boolean = true,
+    public file: string = 'a',
+    public rank: number = 0,
+    public piece: 'pawn' | 'rook' | 'bishop' | 'knight' | 'queen' | 'king' = 'pawn'
+  ) {}
 }
+
 function gridGenerator(width: number, height: number) {
     const cells: number = width * height;
-    const grid: number[] = [];
-    for (let i = 0; i < cells; i++) {
-
-      grid.push(i);
+    const grid: Square[] = [];
+    for (let i = 1; i < cells; i++) {
+      if (i % 2 == 0) {
+        const square = new Square(i,true, false, "b", 2, "rook")
+        grid.push(square);
+      } else {
+        const square = new Square(i,true, true, "b", 2, "rook")
+        grid.push(square);
+      }
+     
     }
     return grid
 }
@@ -22,8 +33,8 @@ const demo = gridGenerator(8,8)
 const chessboard = () => {
 
   return (
-    <div>
-      {demo.map(item => <div>{item}</div>)}
+    <div className={CSS.chessboard}>
+      {demo.map(item => <div className={`${item.white ? CSS.white : CSS.black}`} key={item.id}>{item.piece}</div>)}
     </div>
   )
 }
