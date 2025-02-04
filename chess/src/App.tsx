491787742494dbe75e765d986ec8b82dc7bb976a
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2025-02-04 13:16:32 +0200

    remove deprecated code

--- chess/src/App.tsx
+++ chess/src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
 import Chessboard from './components/Chessboard'
 import CSS from '../src/styles/App.module.css'
 
-type Color =  'white' | 'black';
-
 const App = () => {
-  const [color, setColor] = useState<Color>('white');
   return (
     <div className={CSS.page}>
         <Chessboard/>
