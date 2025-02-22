82963cac292ead2eaa6aaa65322a9f21899acc4f
KestutisKasparavicius <kestutis.kasparavicius@stud.ktmc.lt>
2025-02-22 20:53:01 +0200

    remove blank filtering

--- chess/src/modules/boardSetup.ts
+++ chess/src/modules/boardSetup.ts
@@ -1,11 +1,9 @@
 type BoardItem = { rank: number; file: string; piece?: string; [key: string]: any };
 export default {
     start: (board: BoardItem[]) => {
-        // Filter board to only include specific ranks
         // laikyti gatova chessboarda, kad kai bus daromi veiksmai, naudoti kaip refrence kurie pajudejo ir nebeturi "specialiu" ejimu pvz pawn 2 tile 1st
-        const partialBoard = board.filter(item => item.rank > 6 || item.rank < 3);
-        
-        let populated = partialBoard.map(item => 
+
+        let populated = board.map(item => 
             item.rank === 7 || item.rank === 2 ? { ...item, piece: "pawn" } : item
         );
 
@@ -45,7 +43,6 @@ export default {
                }
             
         })
-
-        console.table(populated);
+        return populated
     }
 };
