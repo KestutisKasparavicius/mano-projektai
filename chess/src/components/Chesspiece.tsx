b7b95b269795aa3bb2827e7fe5828002e38af457
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2025-03-04 19:28:43 +0200

    add loading logic

--- chess/src/components/Chesspiece.tsx
+++ chess/src/components/Chesspiece.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const Chesspiece = () => {
-  return (
-    <div>Rook</div>
-  )
-}
+const Chesspiece = ({selection}: { selection: "pawn" | "rook" | "bishop" | "knight" | "queen" | "king" }) => {
+  const pieces = {
+    pawn: "Pawn",
+    rook: "Rook",
+    bishop: "Bishop",
+    knight: "Knight",
+    queen: "Queen",
+    king: "King",
+  };
+
+  return <>{pieces[selection]}</>;
+};
+
 
 export default Chesspiece
